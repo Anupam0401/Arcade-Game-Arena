f869351546ced8a485606cfb35c74a4c95c74a9c
ruchitsaxena <71293657+ruchitsaxena@users.noreply.github.com>
2020-10-02 23:33:25 +0530

    Update Sudoku_test.c

--- Sudoku_test.c
+++ Sudoku_test.c
@@ -5,7 +5,8 @@
 #include<time.h>
 
 
-int Sudoku_pass(int a[][9], int b[][9]){
+int Sudoku_pass(int a[][9], int b[][9])
+{
 int i,j,k=0,flag=0;
  for(i=0;i<9;i++){
  	for(j=0;j<9;j++){
@@ -42,7 +43,37 @@ int i,j,k=0,flag=0;
  	}
  return 0;
 }
-void sudoku(int a[][9],int n){
+
+int checkSmall(int a[][9],int i,int j, int k)
+{
+ i=(i/3)*3;
+ j=(j/3)*3;
+ int i1=i,j1=j;
+ for(i=i1;i<(i1+3);i++){
+ 	for(j=j1;j<(j1+3);j++){
+ 		if(k==a[i][j]){
+ 			return 0;
+ 			}
+ 		}
+ 	}
+ return 1;
+}
+int check(int a[][9],int i,int j, int k){
+ int i1=0,j1=0;
+ for(;i1<9;i1++){
+ 	if(k==a[i1][j])
+ 		return 0;
+ 	}
+ for(;j1<9;j1++){
+ 	if(k==a[i][j1])
+ 		return 0;
+ 	}
+ return 1;
+}
+
+
+void sudoku(int a[][9],int n)
+{
 printf("Sudoku_created\n");
 	int i,b,c,d;
  srand(time(0));
@@ -68,7 +99,10 @@ printf("Sudoku_created\n");
    
    print(a);
 }
-void playsudoku(int a[][9], int b[][9]){
+
+
+void playsudoku(int a[][9], int b[][9])
+{
 	int r,c,n;
 	printf("Enter row (space) column (space) number:\n");
 	scanf("%d",&r);
@@ -85,7 +119,10 @@ void playsudoku(int a[][9], int b[][9]){
 	if(Sudoku_pass(a,b)==0)
 		playsudoku(a,b);
 }
-int main() {
+
+
+int main() 
+{
  int n,i,j,num,k,a[9][9]={0},b[9][9];
  printf("Enter \n1 for Beginner Level \nor\n 2 for Intermediate level\nor\n 3 for Advanced Level\n");
  scanf("%d",&n);
