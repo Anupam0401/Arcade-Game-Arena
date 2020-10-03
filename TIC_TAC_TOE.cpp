c212f419720e29a4e9f55adf272db5a9cab7a677
Anupam0401 <55993073+Anupam0401@users.noreply.github.com>
2020-10-03 21:38:41 +0530

    added winner() fuction

--- TIC_TAC_TOE.cpp
+++ TIC_TAC_TOE.cpp
@@ -84,8 +84,41 @@ int main()
     O GAME IS OVER AND NO RESULT */
 
 
-//add winner() func here
+//added winner() func here
+int winner()
+{
+    if (square[1] == square[2] && square[2] == square[3])
 
+        return 1;
+    else if (square[4] == square[5] && square[5] == square[6])
+
+        return 1;
+    else if (square[7] == square[8] && square[8] == square[9])
+
+        return 1;
+    else if (square[1] == square[4] && square[4] == square[7])
+
+        return 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+
+        return 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+
+        return 1;
+    else if (square[1] == square[5] && square[5] == square[9])
+
+        return 1;
+    else if (square[3] == square[5] && square[5] == square[7])
+
+        return 1;
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
+                    && square[4] != '4' && square[5] != '5' && square[6] != '6'
+                  && square[7] != '7' && square[8] != '8' && square[9] != '9')
+
+        return 0;
+    else
+        return -1;
+}
 
 
 //FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
