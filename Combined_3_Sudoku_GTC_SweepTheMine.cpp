b46349f7050ac2db7fcff48b06861c10cd028b1e
Abdur-Rahman-Khan <71510046+Abdur-Rahman-Khan@users.noreply.github.com>
2020-10-11 16:40:51 +0530

    Update Combined_3_Sudoku_GTC_SweepTheMine.cpp

--- Combined_3_Sudoku_GTC_SweepTheMine.cpp
+++ Combined_3_Sudoku_GTC_SweepTheMine.cpp
@@ -437,6 +437,82 @@ int SWEEPTHEMINE()
 }
 rn mt;
 }
+int maxt=20;
+int lf (char, string, string&);
+int  GTC()
+{
+	string name;
+	char letter;
+	int nwg=0,i;
+	string word;
+	string words[] ={"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"}; //added country name in this array
+	srand(time(NULL));
+	int n=rand()% 200;
+	word=words[n];
+	for(i = 0; i <=word.length(); i++)
+  word[i] = tolower(word[i]);
+
+	string unknown(word.length(),'*');
+	cout << "\n\nWelcome to GTC...\nGUESS THE COUNTRY\n ";
+	cout << "\n\nEach letter is represented by a *.";
+	cout << "\n\nYou have to type only one letter in one try, only if guess is wrong your guess count will decrease ";
+	 maxt=6;
+	  if( maxt>20)
+	  	 maxt=4;
+	  else if(word.length()>10)
+	  	maxt=5;
+	cout << "\n\nYou have " << maxt << " tries to try and guess the word.";
+	cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+   
+	while (nwg < maxt)
+	{
+		cout << "\n\n" << unknown;
+		cout << "\n\nGuess a letter: ";
+		cin >> letter;
+		if (lf(letter, word, unknown)==0)
+		{
+			cout << endl << "That letter isn't there!" << endl;
+			nwg++;
+		}
+		else
+		{
+			cout << endl << "You found a letter" << endl;
+		}
+		cout << "You have " << maxt - nwg;
+		cout << " guesses left." << endl;
+		if (word==unknown)
+		{	cout << word << endl;
+			cout << "You got it!";
+			break;
+		}
+	}
+	if(nwg == maxt)
+	{
+		cout << "\n you lose" << endl;
+		cout << "The word was : " << word << endl;
+	}
+	cin.ignore();
+	cin.get();
+	return 0;
+}
+
+//letter_find function here
+int lf (char guess, string sw, string &gw)
+{
+	int i;
+	int mt=0;
+	int len=sw.length();
+	for (i = 0; i< len; i++)
+	{
+		if (guess == gw[i])
+			return 0;
+		if (guess == sw[i])
+		{
+			gw[i] = guess;
+			mt++;
+		}
+	}
+	retu
 //functions TO CALL TO PLAY GAMES NAME
 //SUDOKU
 //SWEEPTHEMINE
